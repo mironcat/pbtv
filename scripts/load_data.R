2f418814b42d1425ed84b7b53422484f33b89544
Eugeny Karasev <534temp@gmail.com>
2024-04-02 23:24:47 +0300

    Update load_data.R
    remove old comments

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -9,7 +9,6 @@ paleosib_climzones <- read.csv("data/Boucot_paleosib_zones.csv", sep=";")
 # filtered stages
 stages_psb<-read.csv("data/stages_psb.csv")
 
-# raw_colls<-read.csv("data/paleosib_colls.csv")
 # filtered collections with paleolatitudes see paleosib project
 paleoColls <-read.csv("data/paleoColls_sibplate_only.csv")%>%left_join(stages_psb%>%dplyr::select(stage,stg,mid), by="stage")%>%
   dplyr::select(stage,stg,collection_no,projects, collection_name,early_interval,late_interval,primary_reference,stratcomments,geocomments,  mid,paleolng_scotese2016, paleolat_scotese2016)
@@ -18,8 +17,6 @@ paleoColls <-read.csv("data/paleoColls_sibplate_only.csv")%>%left_join(stages_ps
 occr_sib <- read.csv("data/paleoOccs_sibplate_only.csv")%>%
   dplyr::select(-X)%>%
   distinct_all()
-
-#  filter(collection_no!=192851) #remove Tuksinskaya collection 
 
 
 sib_coord_by_age_scotese<-read.csv('data/sib_coord_by_age_scotese.csv')
@@ -45,32 +42,7 @@ getPoly <- function(dat) {
   )
 }
 sib_poly_scotese<-getPoly(sibcoord_scotese)
-# 
-# episodes<-unique(pcclimzones_scotese$episodes)
-# # episodes<-c("cool_temp1")
-# clim_polygons<-tibble()
-# for (episode in episodes) {
-#   print(episode)
-#   climzone_dat<-pcclimzones_scotese%>%filter(episodes==episode)
-#   clim_poly<- getPoly(climzone_dat)
-#   clim_polygons<-clim_polygons%>%bind_rows(clim_poly)
-# }
-# clim_polygons<-clim_polygons%>%mutate(globPointNum=row_number())
-# write.csv(clim_polygons, 'data/clim_polygons.csv',row.names = F)
 clim_polygons<-read.csv('data/clim_polygons.csv')
-
-
-# y=45-x*0.5
-# y+x*0.5=45
-# x=(45-y)/0.5
-# geom_text(data = sibcoord_scotese, mapping = aes(x = age, y = (45-temp)/0.5, label=temp ) )+
-# geom_hline(yintercept = (45-25)/0.5)
-
-# p+
-#   geom_polygon(data=clim_polygons,mapping = aes(x=age,y=lat,group=episodes, fill=zones),alpha=0.2 )+
-#   geom_point(data = paleoColls, mapping = aes(x=mid,y=paleolat_scotese2016),color="orange")
-
-
 occr_coll_analysis<-occr_sib%>%distinct(collection_no, paleolat_scotese2016, mid)%>%
         group_by(collection_no)%>%
         summarise(n=n())%>%
